b3a723f10d5f1128b7603608fc06c0b8143b01b7
mrminhlunvn <mrminhlunvn1@gmail.com>
2023-08-23 13:34:06 +0700

    Finished psql script and python function for creating the OLTP schema

--- schema_creation.py
+++ schema_creation.py
@@ -1,28 +1,33 @@
 import psycopg2 as pg
 import psycopg2.sql as sql
 import os
+import oltp_schema as oltp
 
 
 def create_oltp_schema():
     """
     Creates the OLTP schema for e-commerce pipeline in 
     olist-ecommerce PostgreSQL database
-    
-    Input file(s): create_oltp_schema.sql
     """
-    conn = pg.connect(
-        database = "olist-ecommerce",
-        user = "postgres",
-        password = "1234"
-    )
-    conn.autocommit = True
-    cur = conn.cursor()
+    try:
+        conn = pg.connect(
+            database = "olist-ecommerce",
+            user = "postgres",
+            password = "1234"
+        )
+        conn.autocommit = True
+        cur = conn.cursor()
+        with open(os.path.join(os.path.dirname(__file__), 'schemas/oltp_schema.sql'), 'r') as script:
+            script = script.read()
+            cur.execute(script)
+            # TODO: Execute commands in 'script' variable
+        # cur.execute(oltp.create_tbl_orders)
+    except:
+        print("Error occurred")
+    finally:
+        conn.close()
 
-    with open(os.path.join(os.path.dirname(__file__), 'schemas/create_oltp_schema.sql'), 'r') as script:
-        script = script.read()
-        # TODO: Execute commands in 'script' variable
 
-    conn.close()
 
 
 if __name__ == '__main__':
